f3a7dbd832538b01bf1e7dc2120627f9a4b6b672
Myth <callumshelton2503@gmail.com>
2025-05-29 07:51:28 +1000

    Version Alpha-0.1.2

---
+++ src/main/java/org/mythic_goose_studios/life/api/v1/Registrator.java
@@ -0,0 +1,24 @@
+package org.mythic_goose_studios.life.api.v1;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class Registrator {
+    public static Item createItem(String name, Item item, String id) {
+        return Registry.register(Registries.ITEM, Identifier.of(id, name), item);
+    }
+
+    private static Block createBlock(String name, Block block, String id) {
+        registerBlockItem(name, block, id);
+        return Registry.register(Registries.BLOCK, Identifier.of(id, name), block);
+    }
+
+    private static void registerBlockItem(String name, Block block, String id) {
+        Registry.register(Registries.ITEM, Identifier.of(id, name),
+                new BlockItem(block, new Item.Settings()));
+    }
+}
